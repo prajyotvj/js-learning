c2a3222a0300761fe6b91e4d64cd55e52a9691af
Prajyot Vinayak Jadhav <112878190+prajyotvj@users.noreply.github.com>
2023-10-11 08:23:47 +0000

    datatypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3)
+
+
+let name ="prajyot"
+let age = 22
+let isLoggedIn = false
+
+/*dataypes*/ 
+// number 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>  ex: let state = null
+// symbol => unique 
+
+// object 
+
+console.log(typeof "prajyot")
+console.log(typeof age)
