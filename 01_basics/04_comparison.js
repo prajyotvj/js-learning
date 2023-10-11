03c33d5b83dacedd3fd185024b9bd70ef5b8fb32
Prajyot Vinayak Jadhav <112878190+prajyotvj@users.noreply.github.com>
2023-10-11 16:15:50 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+let x=5 
+console.log(x==8)   // o/p:false
+console.log(x==5)   // op:true
+console.log(x=="5") // new  op:true 
+
+console.log(x===5)   // "===":equal value and equal type o/p:true
+console.log(x==="5") // o/p:false
+
+console.log(x!=8)    // o/p:true
+console.log(x!==5)   // o/p:false not equal value or not equal type
+console.log(x!=="5") // o/p:true
+console.log(x!==8)   // o/p:true
+
