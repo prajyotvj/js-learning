02c3e0dc94adeba526acbaaad83f1174eed09ee7
Prajyot Vinayak Jadhav <112878190+prajyotvj@users.noreply.github.com>
2023-10-11 16:39:18 +0000

    dataType summary

---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,35 @@
+// Primitive data type
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol,BigInt
+
+
+
+// Reference type(Non primitive): Array, Objects, Functions
+
+const heros =["shaktiman","naagraj","doga"];  //Array 
+
+let myObj={                                //object
+    name:"prajyot",
+    age:22,
+}
+
+const myFunction = function(){       // function
+    console.log("Hello world");
+}
+
+/*
+Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+*/
