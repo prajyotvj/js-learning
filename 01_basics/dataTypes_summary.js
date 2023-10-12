dceb1b5489a4283a456969d15a3fb69f2d8a18fe
Prajyot Vinayak Jadhav <112878190+prajyotvj@users.noreply.github.com>
2023-10-12 05:30:02 +0000

    dataTypes_summary

--- 01_basics/dataTypes_summary.js
+++ 01_basics/dataTypes_summary.js
@@ -1,10 +1,20 @@
-// Primitive data type
+// There are 2 types of datatype
 
-// 7 types : String, Number, Boolean, null, undefined, Symbol,BigInt
+// 1. Primitive datatype:-
+    //     1.String
+    //     2. Number
+    //     3. Boolean (true / false)
+    //     4. null (iska matlab hai empty or zero nahi hai)
+    //     5. undefined (aapne variable declare kardiya hai but usmein aap kya value dalenge aapne decide nahi kiya hai)
+    //     6. Symbol (kisi bhi value ko unique banane ke liye use kiya jata hai)
+    //     7. BigInt (jo badi values hoti hai oo hum BigInt mein use karte hai)
+
+// 2. Reference type(Non primitive): Array, Objects, Functions
+    // 1. Array ( [ ] )
+    // 2. Object ( { } )
+    // 3. Function ( function( ){ } )
 
 
-
-// Reference type(Non primitive): Array, Objects, Functions
 
 const heros =["shaktiman","naagraj","doga"];  //Array 
 
